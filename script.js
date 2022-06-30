e5b8a818b82ac6c0745e8a7a44b462466850b53d
Trevor <trevorgrilley@yahoo.com>
2022-06-30 15:40:30 -0400

    created functions for numbers and special characters

--- script.js
+++ script.js
@@ -28,6 +28,82 @@ function passLength(){
     return passwordLength;
 }
 
+//Uppercase letter function
+function passUppercase(){
+  uppercaseCheck = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
+    uppercaseCheck = uppercaseCheck.toLowerCase();
+
+    if (uppercaseCheck === null || uppercaseCheck === ""){
+      alert("Please answer Yes or No");
+      passUppercase();
+
+    }
+    else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
+      uppercaseCheck = true;
+      return uppercaseCheck;
+
+    }
+    else if (uppercaseCheck === "no" || uppercaseCheck ==="n"){
+      uppercaseCheck = false;
+      return uppercaseCheck;
+    
+    }
+    else {
+      alert("Please answer Yes or No");
+      passUppercase();
+    }
+    return uppercaseCheck;
+}
+
+//Numbers function
+function passNumbers(){
+  numberCheck = prompt("Do you want to include numbers in your password? \n(Yes or No)");
+    numberCheck = numberCheck.toLowerCase();
+
+    if (numberCheck === null || numberCheck === ""){
+      alert("Please answer Yes or No");
+      passNumbers();
+
+    }else if (numberCheck === "yes" || numberCheck ==="y"){
+      numberCheck = true;
+      return numberCheck;
+
+    }else if (numberCheck === "no" || numberCheck ==="n"){
+      numberCheck = false;
+      return numberCheck;
+    
+    }else {
+      alert("Please answer Yes or No");
+      passNumbers();
+    }
+    return numberCheck;
+}
+
+//Special character function
+function passSpecial(){
+  specialCheck = prompt("Do you want to include special characters in your password? \n(Yes or No)");
+    specialCheck = specialCheck.toLowerCase();
+
+    if (specialCheck === null || specialCheck === ""){
+      alert("Please answer Yes or No");
+      passSpecial();
+
+    }else if (specialCheck === "yes" || specialCheck ==="y"){
+      specialCheck = true;
+      return specialCheck;
+
+    }else if (specialCheck === "no" || specialCheck ==="n"){
+      specialCheck = false;
+      return specialCheck;
+    
+    }else {
+      alert("Please answer Yes or No");
+      passSpecial();
+    }
+    return specialCheck;
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
